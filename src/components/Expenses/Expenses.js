8bb3bd573186f4437c48106f74700c9916112af8
fhuss009 <fhuss009@fiu.edu>
2021-08-21 22:39:48 -0500

    Changing components to arrow functions

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,7 +2,7 @@ import ExpenseItem from "./ExpenseItem";
 import './Expenses.css'
 import Card from "../UI/Card";
 
-function Expenses(props){
+const Expenses = (props) => {
     return(
         <Card className="expenses">
             <ExpenseItem id={props.expenses[0].id} title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date}></ExpenseItem>
